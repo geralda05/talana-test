643ac5100632068193130df46f978ed5a3f1eeb6
Jose Paez <jose.paez@tecnoandina.cl>
2019-12-09 00:37:31 -0300

    identacion

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
- <div id="app">
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <Burger></Burger>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        
-         <button data-toggle="modal" data-target="#exampleModal" class="ml-auto btn-success btn"><i class="fa fa-shopping-cart"></i></button>
+        <button data-toggle="modal" data-target="#exampleModal" class="ml-auto btn-success btn"><i class="fa fa-shopping-cart"></i></button>
       </div>
     </nav>
     <div class="row bg-dark" style="min-height:100vh">
